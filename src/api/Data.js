f405d7210377df94fe0fbf68753a6fb36fb3639c
Panik999 <pan.icon74@gmail.com>
2022-09-29 18:59:17 +0200

    Detail Data Array

--- src/api/Data.js
+++ src/api/Data.js
@@ -1,73 +1,176 @@
 let Data = [
   {
-    id: 521,
-    title: "Diablo Immortal",
-    thumbnail: "https://www.freetogame.com/g/521/thumbnail.jpg",
-    short_description:
-      "Built for mobile and also released on PC, Diablo Immortal fills in the gaps between Diablo II and III in an MMOARPG environment.",
-    game_url: "https://www.freetogame.com/open/diablo-immortal",
-    genre: "MMOARPG",
-    platform: "PC (Windows)",
-    publisher: "Blizzard Entertainment",
-    developer: "Blizzard Entertainment",
-    release_date: "2022-06-02",
-    freetogame_profile_url: "https://www.freetogame.com/diablo-immortal",
-  },
-  {
-    id: 517,
-    title: "Lost Ark",
-    thumbnail: "https://www.freetogame.com/g/517/thumbnail.jpg",
-    short_description:
-      "Smilegate’s free-to-play multiplayer ARPG is a massive adventure filled with lands waiting to be explored, people waiting to be met, and an ancient evil waiting to be destroyed.",
-    game_url: "https://www.freetogame.com/open/lost-ark",
-    genre: "ARPG",
-    platform: "PC (Windows)",
-    publisher: "Amazon Games",
-    developer: "Smilegate RPG",
-    release_date: "2022-02-11",
-    freetogame_profile_url: "https://www.freetogame.com/lost-ark",
-  },
-  {
-    id: 516,
-    title: "PUBG: BATTLEGROUNDS",
-    thumbnail: "https://www.freetogame.com/g/516/thumbnail.jpg",
-    short_description:
-      "Get into the action in one of the longest running battle royale games PUBG Battlegrounds.",
-    game_url: "https://www.freetogame.com/open/pubg",
-    genre: "Shooter",
-    platform: "PC (Windows)",
-    publisher: "KRAFTON, Inc.",
-    developer: "KRAFTON, Inc.",
-    release_date: "2022-01-12",
-    freetogame_profile_url: "https://www.freetogame.com/pubg",
-  },
-  {
-    id: 508,
-    title: "Enlisted",
-    thumbnail: "https://www.freetogame.com/g/508/thumbnail.jpg",
-    short_description:
-      "Get ready to command your own World War II military squad in Gaijin and Darkflow Software’s MMO squad-based shooter Enlisted. ",
-    game_url: "https://www.freetogame.com/open/enlisted",
-    genre: "Shooter",
-    platform: "PC (Windows)",
-    publisher: "Gaijin Entertainment",
-    developer: "Darkflow Software",
-    release_date: "2021-04-08",
-    freetogame_profile_url: "https://www.freetogame.com/enlisted",
-  },
-  {
-    id: 345,
-    title: "Forge of Empires",
-    thumbnail: "https://www.freetogame.com/g/345/thumbnail.jpg",
-    short_description:
-      "A free to play 2D browser-based online strategy game, become the leader and raise your city.",
-    game_url: "https://www.freetogame.com/open/forge-of-empires",
-    genre: "Strategy",
-    platform: "Web Browser",
-    publisher: "InnoGames",
-    developer: "InnoGames",
-    release_date: "2012-04-17",
-    freetogame_profile_url: "https://www.freetogame.com/forge-of-empires",
-  },
+    "id": 521,
+    "title": "Diablo Immortal",
+    "thumbnail": "https://www.freetogame.com/g/521/thumbnail.jpg",
+    "status": "Live",
+    "short_description": "Built for mobile and also released on PC, Diablo Immortal fills in the gaps between Diablo II and III in an MMOARPG environment.",
+    "description": "The demon fighting doesn’t have to stop when you walk away from the computer thanks to Blizzard Entertainment’s Diablo Immortal. Built for mobile and also released on PC, the game fills in the gaps between Diablo II and III in an MMOARPG environment.\r\n\r\nDiablo Immortal picks up following the presumed death of the Archangel Tyrael, during which time mankind must deal with the fallout. One of the many problems are the fragments of the shattered Worldstone spread across the land, waiting for Diablo’s underlings to collect them in an attempt to bring about his return.\r\n\r\nPlayers can choose from one of six classes in Diablo Immortal. These are the classic Barbarian, the Crusader, the Demon Hunter, the Monk, the Necromancer, and the Wizard. All six classes features their own unique skills and abilities.\r\n\r\nThe game also introduces six new bosses, ranging from The Skeleton King to the Glacial Colossus. Each offers players a unique challenge, and all are found in places filled with danger. So, players will want to be well prepared before taking them on.\r\n",
+    "game_url": "https://www.freetogame.com/open/diablo-immortal",
+    "genre": "MMOARPG",
+    "platform": "Windows",
+    "publisher": "Blizzard Entertainment",
+    "developer": "Blizzard Entertainment",
+    "release_date": "2022-06-02",
+    "freetogame_profile_url": "https://www.freetogame.com/diablo-immortal",
+    "minimum_system_requirements": {
+    "os": "Windows 7 / Windows 8 / Windows 10 / Windows 11 (64-bit)",
+    "processor": "Intel Core i3 or AMD FX-8100",
+    "memory": "4 GB RAM",
+    "graphics": "NVIDIA GeForce GTX 460, ATI Radeon HD 6850 or Intel HD Graphics 530",
+    "storage": "?"
+    },
+    "screenshots": [
+    {
+    "id": 1277,
+    "image": "https://www.freetogame.com/g/521/diablo-immortal-1.jpg"
+    },
+    {
+    "id": 1278,
+    "image": "https://www.freetogame.com/g/521/diablo-immortal-2.jpg"
+    },
+    {
+    "id": 1279,
+    "image": "https://www.freetogame.com/g/521/diablo-immortal-3.jpg"
+    }
+    ]
+    },
+    {
+      "id": 517,
+      "title": "Lost Ark",
+      "thumbnail": "https://www.freetogame.com/g/517/thumbnail.jpg",
+      "status": "Live",
+      "short_description": "Smilegate’s free-to-play multiplayer ARPG is a massive adventure filled with lands waiting to be explored, people waiting to be met, and an ancient evil waiting to be destroyed.",
+      "description": "Smilegate’s free-to-play multiplayer ARPG is a massive adventure filled with lands waiting to be explored, people waiting to be met, and an ancient evil waiting to be destroyed. In Lost Ark, players enter the world of Arkesia and embark on a journey across several lands in a quest to locate the Ark of Legends. Stop the demon Kazeros and his army before they seize control. And hurry, the demons are searching for the Ark too.\r\n\r\nChoose to play as one of several classes ranging from the sword-wielding warrior to a mage or even the gunner – each with several advanced class options. Play alone or with friends (or acquaintances) to explore seven vast continents filled with strange cultures and creatures. Fight in PvP, explore dungeons, and work together in raids. Decide what you like to do best and go for it – but don’t forget to save the world.\r\n\r\nLost Ark offers an easy-to-learn system with plenty of room to customize. Use the game’s ‘Tripod’ system to unlock up to three tiers of customization for each ability and create the combat style that works best for you.\r\n\r\nAs for other features, Lost Ark offers crafting, guilds, and other systems players expect from online games. Whatever you want to do, it’s up to you.\r\n",
+      "game_url": "https://www.freetogame.com/open/lost-ark",
+      "genre": "ARPG",
+      "platform": "Windows",
+      "publisher": "Amazon Games",
+      "developer": "Smilegate RPG",
+      "release_date": "2022-02-11",
+      "freetogame_profile_url": "https://www.freetogame.com/lost-ark",
+      "minimum_system_requirements": {
+      "os": "Windows 10 (64-bit only)",
+      "processor": "Intel i3 or AMD Ryzen 3",
+      "memory": "8 GB RAM",
+      "graphics": "NVIDIA GeForce GTX 460 / AMD HD6850",
+      "storage": "50 GB available space"
+      },
+      "screenshots": [
+      {
+      "id": 1259,
+      "image": "https://www.freetogame.com/g/517/lost-ark-1.jpg"
+      },
+      {
+      "id": 1260,
+      "image": "https://www.freetogame.com/g/517/lost-ark-2.jpg"
+      },
+      {
+      "id": 1261,
+      "image": "https://www.freetogame.com/g/517/lost-ark-3.jpg"
+      }
+      ]
+      },
+      {
+        "id": 516,
+        "title": "PUBG: BATTLEGROUNDS",
+        "thumbnail": "https://www.freetogame.com/g/516/thumbnail.jpg",
+        "status": "Live",
+        "short_description": "Get into the action in one of the longest running battle royale games PUBG Battlegrounds.",
+        "description": "Get into the action in one of the longest running battle royale games PUBG Battlegrounds. Originally titled PlayerUnknown’s Battlegrounds, the game has been offering players the full battle royale experience in a more realistic setting since 2017. Test yourself against 99 other players on seven different maps using whatever weapons you can find. Just be sure to be the last one standing, because that’s the only way to win.\r\n\r\nGo it alone or play with friends in groups of two or four. Start out with only the clothes on your back, loot buildings, towns, and other locations to obtain the equipment you need. This includes weapons, armor, vehicles, and more. If you want the really good gear – and it’s likely you do – you’ll have to take some risks and head to the more dangerous zones on the map.\r\n\r\nAs is generally the case with the battle royale formula, PUBG’s map shrinks over time, herding all players into a random location where they will eventually have no choice but to engage each other or risk taking damage outside the restricted area. At the same time, random areas of the map will be bombed. So, players must be aware of both events and do their best to avoid them.\r\n\r\nPUBG Battlegrounds is free-to-play and offers players purchasable items in the form of cosmetics. These do not impact game play.\r\n",
+        "game_url": "https://www.freetogame.com/open/pubg",
+        "genre": "Shooter",
+        "platform": "Windows",
+        "publisher": "KRAFTON, Inc.",
+        "developer": "KRAFTON, Inc.",
+        "release_date": "2022-01-12",
+        "freetogame_profile_url": "https://www.freetogame.com/pubg",
+        "minimum_system_requirements": {
+        "os": "64-bit Windows 7, Windows 8.1, Windows 10",
+        "processor": "Intel Core i5-4430 / AMD FX-6300",
+        "memory": "8 GB RAM",
+        "graphics": "NVIDIA GeForce GTX 960 2GB / AMD Radeon R7 370 2GB",
+        "storage": "40 GB available space"
+        },
+        "screenshots": [
+        {
+        "id": 1256,
+        "image": "https://www.freetogame.com/g/516/pubg-1.jpg"
+        },
+        {
+        "id": 1257,
+        "image": "https://www.freetogame.com/g/516/pubg-2.jpg"
+        },
+        {
+        "id": 1258,
+        "image": "https://www.freetogame.com/g/516/pubg-3.jpg"
+        }
+        ]
+        },
+        {
+          "id": 508,
+          "title": "Enlisted",
+          "thumbnail": "https://www.freetogame.com/g/508/thumbnail.jpg",
+          "status": "Live",
+          "short_description": "Get ready to command your own World War II military squad in Gaijin and Darkflow Software’s MMO squad-based shooter Enlisted. ",
+          "description": "Get ready to command your own World War II military squad in Gaijin and Darkflow Software’s squad-based shooter Enlisted. Play as an infantry squad leader, tank crew, or even an aircraft pilot in a game where everything from the uniforms and weapons to the vehicles are designed with historical accuracy in mind.\r\n\r\nIn Enlisted, players can train an equip their squad with weapons and other useful items before taking them into battle. Players control one soldier in their squad at a time, and have the ability to switch when a part of the squad has been defeated. When one solider is under player control, the rest are controlled by AI that obeys player orders.\r\n\r\nPlayers have access to four different campaigns, each associated with a specific real-world battle. In each instance the squads, weaponry, vehicles and more will appropriately correspond to each location and date. The game offers hundreds of realistic weapons as well as vehicles that World War II buffs will be familiar with. \r\n\r\nAs for the squads, players have access to 12 different kinds of specialists. In addition to the soldiers that spawn naturally, players can recruit additional ones.",
+          "game_url": "https://www.freetogame.com/open/enlisted",
+          "genre": "Shooter",
+          "platform": "Windows",
+          "publisher": "Gaijin Entertainment",
+          "developer": "Darkflow Software",
+          "release_date": "2021-04-08",
+          "freetogame_profile_url": "https://www.freetogame.com/enlisted",
+          "minimum_system_requirements": {
+          "os": "Windows 7 / 8 / 10 64bit",
+          "processor": "Intel Core i3 or better",
+          "memory": "8 GB",
+          "graphics": "Nvidia GeForce 660, AMD Radeon series 78XX or better",
+          "storage": "12GB"
+          },
+          "screenshots": [
+          {
+          "id": 1231,
+          "image": "https://www.freetogame.com/g/508/enlisted-1.jpg"
+          },
+          {
+          "id": 1232,
+          "image": "https://www.freetogame.com/g/508/enlisted-2.jpg"
+          },
+          {
+          "id": 1233,
+          "image": "https://www.freetogame.com/g/508/enlisted-3.jpg"
+          }
+          ]
+          },
+          {
+            "id": 345,
+            "title": "Forge of Empires",
+            "thumbnail": "https://www.freetogame.com/g/345/thumbnail.jpg",
+            "status": "Live",
+            "short_description": "A free to play 2D browser-based online strategy game, become the leader and raise your city.",
+            "description": "Forge of Empires is a free to play 2D browser-based online strategy game that gives players the chance to make their unique city-state into an imperial powerhouse. Everyone has dreamt of building their own empire at some point or another, and Forge of Empires can bring that dream to life.\r\n\r\nPlayers will watch as their civilizations in Forge of Empires begin in the Stone Age and advance all the way into the Industrial Age. The campaign mode is single player and brings players a unique in-depth experience that allows them to use military action, skilled diplomacy, and cunning strategy to achieve their goals.\r\n\r\nMoreover, players in Forge of Empires choose where everything goes, setting up their civilization according to their whims and desires. Any buildings they want and where they want them are ultimately up to them.\r\n\r\nThe greatest feature of Forge of Empires is its wide availability. Players can access Forge of Empires from their Browsers, Apple iOS, Android App, and even on Facebook. It’s only fitting that a game that can create an empire reaches across a multitude of platforms.",
+            "game_url": "https://www.freetogame.com/open/forge-of-empires",
+            "genre": "Strategy",
+            "platform": "Web Browser",
+            "publisher": "InnoGames",
+            "developer": "InnoGames",
+            "release_date": "2012-04-17",
+            "freetogame_profile_url": "https://www.freetogame.com/forge-of-empires",
+            "screenshots": [
+            {
+            "id": 909,
+            "image": "https://www.freetogame.com/g/345/forge-of-empires-1.jpg"
+            },
+            {
+            "id": 910,
+            "image": "https://www.freetogame.com/g/345/forge-of-empires-2.jpg"
+            },
+            {
+            "id": 911,
+            "image": "https://www.freetogame.com/g/345/forge-of-empires-3.jpg"
+            }
+            ]
+            }
 ];
 export default Data;
