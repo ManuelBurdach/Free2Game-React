6de832a9e13430b80662d56f6adc6c80752db646
“cinalselin” <hello@selincinal.de>
2022-09-29 20:31:30 +0200

    added css for allgames page

--- src/pages/allGames/allGames.js
+++ src/pages/allGames/allGames.js
@@ -1,28 +1,34 @@
 // IMPORT COMPONENTS
 import GameCards from "../../Components/gamesCard/GameCards";
-// import nav, header, dropdown menü TODO
+import hierkommtdienav from "../../Components/nav";
+import hierkommtderheader from "../../Components/header";
+import hierkommtdropdown from "../../Components/dropdownMenu/";
 
 // IMPORT DATA
 import Data from "../../api/Data";
 
+// IMPORT CSS
+import "./AllGames.css";
+
 const AllGames = () => {
-  return;
-  <>
-    {/* Hier kommt die Nav */};{/* Hier kommt der Header */};
-    {/* Hier kommt das Dropdownmenü hin */}
-    <div className="all-games-container">
-      {Data.map((Data, index) => (
-        <GameCards
-          key={"Game" + index}
-          thumbnail={Data.thumbnail}
-          title={Data.title}
-          short_description={Data.short_description}
-          platform={Data.platform}
-          genre={Data.genre}
-        />
-      ))}
-    </div>
-  </>;
+  return (
+    <>
+      {/* Hier kommt die Nav */};{/* Hier kommt der Header */};
+      {/* Hier kommt das Dropdownmenü hin */}
+      <div className="all-games-container">
+        {Data.map((Data, index) => (
+          <GameCards
+            key={"Game" + index}
+            thumbnail={Data.thumbnail}
+            title={Data.title}
+            short_description={Data.short_description}
+            platform={Data.platform}
+            genre={Data.genre}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default AllGames;
