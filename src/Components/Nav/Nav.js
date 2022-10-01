0320f6c282e9d14122ac3b69b10b3cd81e65d496
“adenjenny” <aden.jennifer@gmail.com>
2022-10-01 12:34:28 +0200

    Neue Nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,47 +1,47 @@
 import "./Nav.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import Games_icon from "../../Assets/Icons/Games_icon.svg";
 import Recently_added_icon from "../../Assets/Icons/Recently_added_icon.svg";
 import Home_icon from "../../Assets/Icons/Home_icon.svg";
+import React, { useState } from "react";
+import { RiMenuLine } from "react-icons/ri";
 
-const Nav = () => {
+function Sidebar() {
+  const sidebarCollapsed = localStorage.getItem("sidebar-collapsed");
+  const [isExpanded, setIsExpanded] = useState(sidebarCollapsed ? false : true);
+
+  const handleToggler = () => {
+    if (isExpanded) {
+      setIsExpanded(false);
+      localStorage.setItem("sidebar-collapsed", true);
+      return;
+    }
+    setIsExpanded(true);
+    localStorage.removeItem("sidebar-collapsed");
+  };
+
   return (
-    <div>
-      <nav>
-        <div className="hamburger-menu">
-          <input id="menu__toggle" type="checkbox" />
-          <label className="menu__btn" htmlFor="menu__toggle">
-            <span></span>
-            <div className="test">
-              <img src={Home_icon} alt="hi"></img>
-
-              <Link to="/AllGames">
-                <img src={Games_icon} alt="hi"></img>
-              </Link>
-
-              <img src={Recently_added_icon} alt="hi"></img>
-            </div>
-          </label>
-
-          <ul className="menu__box">
-            <li className="menu__item">
-              <img src={Home_icon} alt="hi"></img>
-              <h3 className="nav-h3"> Home</h3>
-            </li>
-            <li className="menu__item">
-              <img src={Games_icon} alt="hi"></img>
-              <h3 className="nav-h3"> All Games</h3>
-            </li>
-            <li className="menu__item">
-              <img src={Recently_added_icon} alt="hi"></img>
-              <h3 className="nav-h3"> Recently Added</h3>
-            </li>
-          </ul>
+    <div className={isExpanded ? "Sidebar" : "Sidebar collapsed"}>
+      <div className="sidebar-header">
+        <RiMenuLine className="sidebar-icon" onClick={handleToggler} />
+      </div>
+      <div className="sidebar-items">
+        <div className="item">
+          <img src={Home_icon}></img>
+          <h3 className="sidebar-text">All Games</h3>
         </div>
-      </nav>
+        <div className="item">
+          <img src={Games_icon}></img>
+          <h3 className="sidebar-text">All Games</h3>
+        </div>
+        <div className="item">
+          <img src={Recently_added_icon}></img>
+          <h3 className="sidebar-text">All Games</h3>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
-export default Nav;
+export default Sidebar;
