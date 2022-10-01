ff9bff7026a294be363c47591a78e5986de18148
Manuel Burdach <manuelburdach@outlook.de>
2022-10-01 09:54:59 +0200

    Upload App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+//Import Css
 import "./App.css";
+
+//Import Components
 import Nav from "./Components/Nav/Nav.js";
+
+//Import Brouser Router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element="" />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
