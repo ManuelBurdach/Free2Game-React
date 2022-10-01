6259d59c1cc4b1b694ef24eb9b68a1d7fa9de32c
Panik999 <pan.icon74@gmail.com>
2022-10-01 14:57:47 +0200

    searchbar in Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,41 @@
 import Rect15 from "../../Assets/Img/Rect15.png";
 import Rect16 from "../../Assets/Img/Rect16.png";
+import Elipse from "../../Assets/Img/Ellipse 2.png";
+import Line from "../../Assets/Img/Line 5.png";
 import Data from "../../Api/Data";
-import "./Header.css"; 
+import "./Header.css";
 
 const Header = () => {
   // const headImg = Data[0].screenshots.image;
-   let randomImg = Math.floor(Math.random() * Data.length);
-   let randomImg2 = Math.floor(Math.random() * 3);
+  let randomImg = Math.floor(Math.random() * Data.length);
+  let randomImg2 = Math.floor(Math.random() * 3);
   return (
     <header>
-      
       <div className="topBar">
         <div className="topBarLogo">
-          <div className="sign">        <img src={Rect15} alt="" />
-        <img src={Rect16} alt="" /></div>
+          <div className="sign">
+            {" "}
+            <img src={Rect15} alt="" />
+            <img src={Rect16} alt="" />
+          </div>
 
-        <h1>FREE2GAME</h1>
-        <h2>{""}</h2>
+          <h1>FREE2GAME</h1>
+          <h2>{""}</h2>
+        </div>
+        <div className="searchBar">
+          <input
+            type="text"
+            id="header-search"
+
+            name="s"
+          />
+          <img src={Elipse} alt="" />
+          <img src={Line} alt="" />
+        </div>
       </div>
-      <input
-        type="text"
-        id="header-search"
-        placeholder="Search Game"
-        name="s"
-      />{" "}
-      </div>
-      <section className="heroSection"><img src={ Data[randomImg].screenshots[randomImg2].image 
-} alt="" /></section>
-      
+      <section className="heroSection">
+        <img src={Data[randomImg].screenshots[randomImg2].image} alt="" />
+      </section>
     </header>
   );
 };
