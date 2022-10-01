45572b56aba168f79331789be13dc58b6171e3f6
Manuel Burdach <manuelburdach@outlook.de>
2022-10-01 14:11:50 +0200

    Upload

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,17 +1,24 @@
-import GamesCards from "../../Components/GamesCard/GameCards";
+import GameCards from "../../Components/GamesCard/GameCards";
 import Data from "../../Api/Data";
+import "./Home.css";
 
 const Home = () => {
-  return Data.map((Data, index) => (
-    <GamesCards
-      key={"gamesCard" + index}
-      img={Data.thumbnail}
-      title={Data.title}
-      short_description={Data.short_description}
-      platform={Data.platform}
-      CarYear={Data.CarYear}
-    />
-  ));
+  return (
+    <>
+      <section className="recentlyadded">
+        {Data.map((Data, index) => (
+          <GameCards
+            key={"gamesCard" + index}
+            img={Data.thumbnail}
+            title={Data.title}
+            short_description={Data.short_description}
+            platform={Data.platform}
+            CarYear={Data.CarYear}
+          />
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default Home;
