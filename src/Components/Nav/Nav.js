8fd068c3afb6a67f7f32ab4646b2df2bd6dc92bf
“adenjenny” <aden.jennifer@gmail.com>
2022-10-01 11:16:41 +0200

    Änderung

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import "./Nav.css";
+import { Link } from "react-router-dom";
 
 import Games_icon from "../../Assets/Icons/Games_icon.svg";
 import Recently_added_icon from "../../Assets/Icons/Recently_added_icon.svg";
@@ -9,29 +10,31 @@ const Nav = () => {
     <div>
       <nav>
         <div className="hamburger-menu">
-          <input id="menu__toggle" onClick={click()} type="checkbox" />
+          <input id="menu__toggle" type="checkbox" />
           <label className="menu__btn" htmlFor="menu__toggle">
             <span></span>
-            <div className="boxshadow" id="bla">
-              <div className="test">
-                <img src={Home_icon} alt="hi"></img>
+            <div className="test">
+              <img src={Home_icon} alt="hi"></img>
 
+              <Link to="/AllGames">
                 <img src={Games_icon} alt="hi"></img>
-                <img src={Recently_added_icon} alt="hi"></img>
-              </div>
-            </div>
+              </Link>
 
-            {/* <Link to="/">{Home_icon}</Link> */}
+              <img src={Recently_added_icon} alt="hi"></img>
+            </div>
           </label>
 
           <ul className="menu__box">
             <li className="menu__item">
+              <img src={Home_icon} alt="hi"></img>
               <h3 className="nav-h3"> Home</h3>
             </li>
             <li className="menu__item">
+              <img src={Games_icon} alt="hi"></img>
               <h3 className="nav-h3"> All Games</h3>
             </li>
             <li className="menu__item">
+              <img src={Recently_added_icon} alt="hi"></img>
               <h3 className="nav-h3"> Recently Added</h3>
             </li>
           </ul>
@@ -39,10 +42,6 @@ const Nav = () => {
       </nav>
     </div>
   );
-};
-
-let click = () => {
-  document.getElementByClassName("boxshadow").remove();
 };
 
 export default Nav;
