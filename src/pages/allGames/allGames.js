d942acb0e3acdbf91563f8acd197b81f3c13431a
“cinalselin” <hello@selincinal.de>
2022-10-01 10:10:30 +0200

    gamecards

--- src/pages/allGames/allGames.js
+++ src/pages/allGames/allGames.js
@@ -12,25 +12,50 @@ import Data from "../../Api/Data";
 import "./AllGames.css";
 
 const AllGames = () => {
-  return (
-    <>
-      <Nav />
-      <Header />
-      {/* Hier kommt das Dropdownmenü hin */}
-      <div className="all-games-container">
-        {Data.map((Data, index) => (
-          <GameCards
-            key={"Game" + index}
-            thumbnail={Data.thumbnail}
-            title={Data.title}
-            short_description={Data.short_description}
-            platform={Data.platform}
-            genre={Data.genre}
-          />
-        ))}
-      </div>
-    </>
-  );
+  if ((user.checked = false)) {
+    // checkbox muss definiert werden und dann mit USER ersetzen
+    return (
+      <>
+        <Nav />
+        <Header />
+        {/* Hier kommt das Dropdownmenü hin */}
+
+        <div className="all-games-container">
+          {Data.map((Data, index) => (
+            <GameCards
+              key={"Game" + index}
+              thumbnail={Data.thumbnail}
+              title={Data.title}
+              short_description={Data.short_description}
+              platform={Data.platform}
+              genre={Data.genre}
+            />
+          ))}
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Nav />
+        <Header />
+        {/* Hier kommt das Dropdownmenü hin */}
+        <p className="search-value">{value.checkbox}</p> <span></span>
+        <div className="all-games-container">
+          {Data.map((Data, index) => (
+            <GameCards
+              key={"Game" + index}
+              thumbnail={Data.thumbnail}
+              title={Data.title}
+              short_description={Data.short_description}
+              platform={Data.platform}
+              genre={Data.genre}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
 };
 
 export default AllGames;
