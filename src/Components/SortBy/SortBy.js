4ec3ce738728bc9c7390b5b6a94ca1db74fd209a
Manuel Burdach <manuelburdach@outlook.de>
2022-10-01 10:33:59 +0200

    Upload SortBy

---
+++ src/Components/SortBy/SortBy.js
@@ -0,0 +1,12 @@
+// Import Css
+import "./SortBy.css";
+
+const SortBy = (props) => {
+  return (
+    <form>
+      <label htmlFor="">{props.title}</label>
+    </form>
+  );
+};
+
+export default SortBy;
