5bb43ff8c00a61540ada6073a61854f7344c2adc
Manuel Burdach <manuelburdach@outlook.de>
2022-10-06 19:22:38 +0200

    Upload

--- src/Pages/Detail/Detail.js
+++ src/Pages/Detail/Detail.js
@@ -7,12 +7,34 @@ import Data from "../../Api/Data";
 // IMPORT CSS
 import "../Detail/Detail.css";
 
+// IMPORT useState, useEffect
+import { useState, useEffect } from "react";
+
 const Detail = () => {
   let params = useParams();
-  let props = Data[params["*"]];
-  return (
+  // let props = Data[params["*"]];
+
+  const [props, setGameId] = useState(null);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+        "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    };
+
+    fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${params["*"]}`, options)
+      .then((response) => response.json())
+      .then((response) => setGameId(response))
+      .catch((err) => console.error(err));
+  }, []);
+
+  console.log(props);
+
+  return props != null ? (
     <div className="Detail">
-      {/* Header? */}
       <main>
         <h1>{props.title}</h1>
         <section className="main-container">
@@ -27,16 +49,8 @@ const Detail = () => {
             <p>{props.description}</p>
           </article>
         </section>
-        <img
-          src={props.screenshots[0].image}
-          alt=""
-          className="detail-img"
-        ></img>
-        <img
-          src={props.screenshots[1].image}
-          alt=""
-          className="detail-img"
-        ></img>
+        <img src={props.screenshots[0].image} alt="" className="detail-img"></img>
+        <img src={props.screenshots[1].image} alt="" className="detail-img"></img>
 
         <section className="main-container">
           <article className="article-left">
@@ -91,6 +105,8 @@ const Detail = () => {
         </section>
       </main>
     </div>
+  ) : (
+    <></>
   );
 };
 
